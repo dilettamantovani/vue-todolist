7091155ba2e3b751e62bf84e009879dd6cf1cc4e
dilettits <mantovani.d3@gmail.com>
2023-11-20 09:44:36 +0100

    consegna esercizio

--- js/main.js
+++ js/main.js
@@ -6,21 +6,27 @@ createApp({
             list: [
                 {
                     task: "finire l'esercizio",
+                    completed: true
                 },
                 {
                     task: "seguire lezione",
+                    completed: false
                 },
                 {
                     task: "cucinare il pranzo",
+                    completed: false
                 },
                 {
                     task: "andare a fare la spesa",
+                    completed: true
                 },
                 {
                     task: "svenire nel lettoe",
+                    completed: false
                 },
                 {
                     task: "andarsi ad allenare",
+                    completed: false
                 },
 
             ],
@@ -28,13 +34,19 @@ createApp({
         }
     },
     methods: {
+        taskStatus(condition) {
+            return (condition) ? "barred" : "";
+        },
         removeTask(index) {
             this.list.splice(index, 1);
         },
+        toggleCompletion(index) {
+            this.list[index].completed = !this.list[index].completed
+        },
         addTask() {
-            this.list.push({task: this.newTask})
+            this.list.push({task: this.newTask, completed: false})
         if (this.newTask == "") {
-            this.list.pop({task: this.newTask})
+            this.list.pop({task: this.newTask, completed: false})
         }
         this.newTask = ""
         
